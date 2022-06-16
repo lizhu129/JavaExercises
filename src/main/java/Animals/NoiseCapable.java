8cf2d3ec81192c0dc7817a0d2b8c677ce5ef7903
lizhu129 <lizhu129@gmail.com>
2022-06-16 21:12:11 +0200

    new exercise inheritance and interface

---
+++ src/main/java/Animals/NoiseCapable.java
@@ -0,0 +1,6 @@
+package Animals;
+
+public interface NoiseCapable {
+
+    void makeNoise();
+}
---
+++ src/main/java/Animals/main.java
@@ -0,0 +1,13 @@
+package Animals;
+
+public class main {
+    public static void main(String[] args) {
+        NoiseCapable dog = new Dog();
+        dog.makeNoise();
+
+        NoiseCapable cat = new Cat("Garfield");
+        cat.makeNoise();
+        Cat c = (Cat) cat;
+        c.purr();
+    }
+}
