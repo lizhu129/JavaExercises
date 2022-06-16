f805668ce58dc9d9863de3b8c25cd094b15e9e4f
lizhu129 <lizhu129@gmail.com>
2022-06-16 13:34:26 +0200

    new exercise

---
+++ src/main/java/exercise/Student.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class Student extends Person {
+    private int credits;
+
+    public Student(String name, int age, int credits) {
+        super(name, age);
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + credits + " credits";
+    }
+
+}
--- src/main/java/exercise1/main.java
+++ src/main/java/exercise1/main.java
@@ -1,3 +1,5 @@
+package exercise1;
+
 public class main {
 
     public static void main(String[] args) {
