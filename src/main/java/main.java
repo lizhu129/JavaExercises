990fe29f3fd929d38ec5022e555f820cdf06bc79
lizhu129 <lizhu129@gmail.com>
2022-06-16 11:58:18 +0200

    test: check if an array contains duplicated numbers

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,23 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        int[] nums = {1, 3, 2, 4, 5, 5, 7, 8, 9, 10, 9};
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                    break;
+                }
+                continue;
+            }
+            continue;
+        }
+        if (count == 0) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
 
 
